2590251ccd27ad4fd5e4dd2cf7c3d8329ce017f9
skeru <ultrakeru-github@yahoo.com>
2015-01-24 05:52:00 +0100

    FPS template

---
+++ HLif3/Source/HLif3/Private/HLif3GameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+#include "HLif3.h"
+#include "HLif3GameMode.h"
+#include "HLif3HUD.h"
+#include "HLif3Character.h"
+
+AHLif3GameMode::AHLif3GameMode(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Blueprints/MyCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = AHLif3HUD::StaticClass();
+}
---
+++ HLif3/Source/HLif3/Public/HLif3GameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameMode.h"
+#include "HLif3GameMode.generated.h"
+
+UCLASS(minimalapi)
+class AHLif3GameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	AHLif3GameMode(const FObjectInitializer& ObjectInitializer);
+};
+
+
+
