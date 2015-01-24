667cafaebbe3fbf027169634c6048b667b8e118d
skeru <ultrakeru-github@yahoo.com>
2015-01-24 15:59:21 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	HLif3/Intermediate/CachedAssetRegistry.bin
    	HLif3/Saved/Config/Windows/EditorUserSettings.ini

---
+++ HLif3/Source/HLif3/Private/CorruptionManager.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "HLif3.h"
+#include "CorruptionManager.h"
+
+
+//////////////////////////////////////////////////////////////////////////
+// CorruptionManager
+
+ACorruptionManager::ACorruptionManager(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	CorruptionLevel = 0.f;
+	DestructibleIndex = -1;
+	OnCorruptionUpdate();
+}
+
+void ACorruptionManager::OnCorruptionIncrease(float Value)
+{
+	if (Value != 0.0f)
+	{
+		// add movement in that direction
+		CorruptionLevel += Value;
+		
+		DestructibleIndex = (rand() % (uint32) DestructiblesLenght);
+		
+		OnCorruptionUpdate();
+
+		if (IsCorruptionOver(m_maxCorruption))
+		{
+			OnGameEnd();
+		}
+	}
+}
+
+bool ACorruptionManager::IsCorruptionOver(float Value)
+{
+	return CorruptionLevel > Value;
+}
+
+bool ACorruptionManager::CanDestroy()
+{
+	return DestructibleIndex >= 0 && DestructiblesLenght > 0;
+}
+
